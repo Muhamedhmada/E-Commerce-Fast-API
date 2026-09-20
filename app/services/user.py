1656a8ef29d8c96c6a07f2b5c9c3eaca144d4a06
Hamada <hamada.dev33@gmail.com>
2026-09-20 19:52:33 +0300

    create endpoint to fetch all users

--- app/services/user.py
+++ app/services/user.py
@@ -1,5 +1,8 @@
-from app.repositories.user import find_user
+from app.repositories import user as repo 
 
 
 def get_user(user_id: int):
-    return find_user(user_id)
+    return repo.find_user(user_id)
+
+def get_users():
+    return repo.get_users()
